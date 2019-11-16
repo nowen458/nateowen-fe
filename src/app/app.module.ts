9149bbd525b2ea9b26f4e7cffd3536d025f3e85e
Nate Owen <nowen747@gmail.com>
2019-11-16 17:11:51 -0500

    Created new components and set up routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,17 +9,37 @@ import { FooterComponent } from './footer/footer.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AboutpageComponent } from './aboutpage/aboutpage.component';
+import { BlogpageComponent } from './blogpage/blogpage.component';
+import { ProjectspageComponent } from './projectspage/projectspage.component';
+import { ContactpageComponent } from './contactpage/contactpage.component';
+
+const appRoutes: Routes = [
+  { path: 'home',     component: HomepageComponent},
+  { path: 'about',    component: AboutpageComponent },
+  { path: 'blog',     component: BlogpageComponent },
+  { path: 'projects', component: ProjectspageComponent },
+  { path: 'contact',  component: ContactpageComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomepageComponent
+    HomepageComponent,
+    AboutpageComponent,
+    BlogpageComponent,
+    ProjectspageComponent,
+    ContactpageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes
+    ),
     NgbModule
   ],
   providers: [],
